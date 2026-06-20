562e50312a8069f7e1473fcdc3d2e81f307c9fda
Diego <diegobattiston014@gmail.com>
2026-06-20 23:14:55 +0200

    test: configura H2 em memoria para testes CI/CD

--- routine/src/test/java/com/habts/routine/RoutineApplicationTests.java
+++ routine/src/test/java/com/habts/routine/RoutineApplicationTests.java
@@ -2,8 +2,10 @@ package com.habts.routine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RoutineApplicationTests {
 
 	@Test
